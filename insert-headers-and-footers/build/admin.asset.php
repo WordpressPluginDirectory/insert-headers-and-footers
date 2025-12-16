ac2aa18ac3a5667fa54660fb160badff80a4d44f
Justin Gardner <justin@rhynorater.com>
2025-12-16 06:02:32 -0500

    2.3.2.1

--- insert-headers-and-footers/build/admin.asset.php
+++ insert-headers-and-footers/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '35dc5dcc78ed91c78e03');
+<?php return array('dependencies' => array('jquery'), 'version' => 'a517239f4723674083d0');
